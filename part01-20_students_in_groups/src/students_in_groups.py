1cd00ccf3457f48448974f19b1bf0691b414885a
TejaskumarReddy <tejaskumarreddy237@gmail.com>
2026-09-05 22:05:17 +0530

    Add files via upload

---
+++ part01-20_students_in_groups/src/students_in_groups.py
@@ -0,0 +1,5 @@
+# Write your solution here
+students=int(input("How many students"))
+group_size=int(input("Desired group size?"))
+groups=(students + group_size - 1)/group_size
+print(f"Number of groups formed: {groups}")
---
+++ part01-23_soup_or_no_soup/src/soup_or_no_soup.py
@@ -0,0 +1,6 @@
+# Write your solution here
+name=input("What is your name?")
+if name != "Jerry":
+    portions=int(input("How many portions?"))
+    print(f"The total cost is {portions*5.90}")
+print("Next please!")
